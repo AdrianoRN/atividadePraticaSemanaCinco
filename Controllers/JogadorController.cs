6714d1c251d8a29c3aed6ece6afc38fcf165c801
Adriano Ribeiro <adrianoribeiro589@gmail.com>
2022-06-15 12:06:09 -0300

    Atividades C Sharp Faculdade

---
+++ Controllers/JogadorController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace atividadePraticaSemanaCinco.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogadorController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            List<Jogador> ListaJogador = new List<Jogador>();
+
+            ListaJogador.Add(new Jogador("João Paulo", 29));
+
+            Jogador novaJogador = new Jogador();
+            novaJogador.setNome("Chewbacca");
+            novaJogador.setIdade(190);
+            ListaJogador.Add(novaJogador);
+
+            foreach (var Jogador in ListaJogador)
+            {
+                Console.WriteLine("Jogador: {0}, {1} anos", Jogador.getNome(), Jogador.getIdade());
+            }
+
+            return novaJogador.getNome();
+        }
+    }
+}
